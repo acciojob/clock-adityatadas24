13d72343d8bfbb41ccde214b7be6db56642688e4
aditya tadas <141910946+adityatadas24@users.noreply.github.com>
2023-09-28 20:22:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+ function updateTimer() {
+      const timerElement = document.getElementById("timer");
+      const currentDate = new Date();
+      const formattedDate = currentDate.toLocaleDateString();
+      const formattedTime = currentDate.toLocaleTimeString();
+
+    
+      timerElement.textContent = `Date: ${formattedDate} | Time: ${formattedTime}`;
+    }
+
+  
+    updateTimer();
+
+    
+    setInterval(updateTimer, 1000);
